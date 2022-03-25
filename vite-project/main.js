854ec4d8be850877c541bdc48e10de0b80efc857
Billycai04 <90789882+Billycai04@users.noreply.github.com>
2022-03-25 13:11:09 -0400

    11

--- vite-project/main.js
+++ vite-project/main.js
@@ -7,8 +7,8 @@ const tl = gsap.timeline({ scrollTrigger: ".header", delay: 0.2 });
 tl.from(".title", { opacity: 0, duration: 0.2, ease: "ease-out" });
 tl.from(".header-desc", { x: -500, duration: 0.2, ease: "ease-out" });
 
-`tl.from(".background-img", {
-  clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+tl.from(".header-background", {
+  clipPath: "polygon(% 0%, 100% 0%, 100% 100%, 0% 100%)",
   duration: 0.5,
   ease: "ease-out",
-});`;
+});
