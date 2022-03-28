836095196558c9c67216b1606f10bb83112c2696
Billycai04 <90789882+Billycai04@users.noreply.github.com>
2022-03-28 13:11:53 -0400

    11

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,8 +4,8 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({ scrollTrigger: ".header", delay: 0.2 });
 
-tl.from(".title", { opacity: 0, duration: 0.2, ease: "ease-out" });
-tl.from(".header-desc", { x: -500, duration: 0.2, ease: "ease-out" });
+tl.from(".navbar", { x: -2000, duration: 0.4, ease: "ease-out" });
+tl.from(".title", { opacity: 0, duration: 0.6, ease: "ease-out" });
 
 `tl.from(".background-img", {
   clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
