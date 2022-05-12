897ce2cfc10e5034245db0cc88998c8e3f89db79
brucem24 <90789913+brucem24@users.noreply.github.com>
2022-05-12 12:48:54 -0400

    NAVBAR

--- vite-project/main.js
+++ vite-project/main.js
@@ -6,8 +6,8 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({ scrollTrigger: ".header", delay: 0.1 });
 
-tl.from(".title", { y: -500, duration: 1, ease: "ease-out" });
+//tl.from(".title", { y: -500, duration: 1, ease: "ease-out" });
 
-tl.from(".navbar", { y: -100, duration: 0.5, ease: "ease-out" });
+//tl.from(".navbar", { y: -100, duration: 0.5, ease: "ease-out" });
 
-tl.from(".navbar-btn", { x: -1500, duration: 0.5, ease: "ease-out", delay: 0 });
+//tl.from(".navbar-btn", { x: -1500, duration: 0.5, ease: "ease-out", delay: 0 });
