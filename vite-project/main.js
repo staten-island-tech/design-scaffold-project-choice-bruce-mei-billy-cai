a367ef508569c39db53abf69ff2f94cc2f902876
brucem24 <90789913+brucem24@users.noreply.github.com>
2022-05-17 13:11:38 -0400

    SCROLL ANIMATIONS AND CSS HOVER

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,10 +4,12 @@ import "./styles/navbar.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-const tl = gsap.timeline({ scrollTrigger: ".header", delay: 0.1 });
+const tl = gsap.timeline({ scrollTrigger: ".title", delay: 0.2 });
 
-tl.from(".title", { y: -500, duration: 0.5, ease: "ease-out" });
+tl.from(".title", { opacity: 0, duration: 0.5 });
 
 tl.from(".navbar", { y: -100, duration: 0.5, ease: "ease-out" });
 
-tl.from(".navbar-btn", { x: -1500, duration: 0.5, ease: "ease-out", delay: 0 });
+tl.from(".navbar-btn", { opacity: 0, duration: 0.5 });
+
+tl.from(".card", { x: -1000, duration: 0.5, ease: "ease-out" });
