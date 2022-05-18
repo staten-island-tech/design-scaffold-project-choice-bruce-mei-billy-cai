a5f26878df83c1f58b7bd33dc48428c43179111b
brucem24 <90789913+brucem24@users.noreply.github.com>
2022-05-18 13:11:12 -0400

    HOVER

--- vite-project/main.js
+++ vite-project/main.js
@@ -8,8 +8,8 @@ const tl = gsap.timeline({ scrollTrigger: ".title", delay: 0.2 });
 
 tl.from(".title", { opacity: 0, duration: 0.5 });
 
-tl.from(".navbar", { y: -100, duration: 0.5, ease: "ease-out" });
+tl.from(".navbar", { y: -150, duration: 0.5, ease: "ease-out" });
 
-tl.from(".navbar-btn", { opacity: 0, duration: 0.5 });
+tl.from(".navbar-btn", { opacity: 0, duration: 1 });
 
-tl.from(".card", { x: -1000, duration: 0.5, ease: "ease-out" });
+tl.from(".card", { x: -1000, duration: 0.5, ease: "ease-in" });
