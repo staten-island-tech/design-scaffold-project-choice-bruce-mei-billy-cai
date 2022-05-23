0ad14fdf387e2b74a4c738dc61fde48c3cc328ce
brucem24 <90789913+brucem24@users.noreply.github.com>
2022-05-23 12:40:48 -0400

    REMOVE TEST

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,12 +4,6 @@ import "./styles/navbar.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-const tl = gsap.timeline({ scrollTrigger: ".title", delay: 0.2 });
+const tl = gsap.timeline({ scrollTrigger: ".card", delay: 0.2 });
 
-tl.from(".title", { opacity: 0, duration: 0.5 });
-
-tl.from(".navbar", { y: -150, duration: 0.5, ease: "ease-out" });
-
-tl.from(".navbar-btn", { opacity: 0, duration: 1 });
-
-tl.from(".card", { x: -3000, duration: 0.5, ease: "ease-in" });
+tl.from(".card", { x: -3000, duration: 2, ease: "ease-in" });
