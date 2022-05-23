b4273d947cc603110ce8300e887751ae8c95c2f4
brucem24 <90789913+brucem24@users.noreply.github.com>
2022-05-23 12:47:21 -0400

    SECOND

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,9 +1,28 @@
 import "./styles/style.css";
 import "./styles/footer.css";
 import "./styles/navbar.css";
+import "./styles/var.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
+
+//GSAP SCROLLTRIGGER
 const tl = gsap.timeline({ scrollTrigger: ".card", delay: 0.2 });
 
 tl.from(".card", { x: -3000, duration: 2, ease: "ease-in" });
+
+//THEMES
+
+document.querySelector(".light").addEventListener("click", function () {
+  {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
+
+document.querySelector(".dark").addEventListener("click", function () {
+  {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  }
+});
