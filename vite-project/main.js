1823b900d98c69794569b0124926fd9c95e68469
brucem24 <90789913+brucem24@users.noreply.github.com>
2022-05-24 13:12:14 -0400

    fleX

--- vite-project/main.js
+++ vite-project/main.js
@@ -12,7 +12,6 @@ const tl = gsap.timeline({ scrollTrigger: ".card", delay: 0.2 });
 tl.from(".card", { x: -3000, duration: 2, ease: "ease-in" });
 
 //THEMES
-
 document.querySelector(".light").addEventListener("click", function () {
   {
     document.body.classList.add("light");
